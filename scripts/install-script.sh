78c17db782f0245b7b7d601bf8186349bd6cbfbc
Earnest Angel <fareast.sn@gmail.com>
2025-09-28 06:04:59 +0800

    [WIP] Include fastcgi wrap in install script

--- scripts/install-script.sh
+++ scripts/install-script.sh
@@ -2,7 +2,7 @@
 
 # [SETUP] Install necessary packages, including git
 echo -e "[SETUP] Install packages"
-apt-get update -qq > /dev/null 2>&1 && apt-get install -qq > /dev/null 2>&1 -y git wget perl perl-doc
+apt-get update -qq > /dev/null 2>&1 && apt-get install -qq > /dev/null 2>&1 -y git wget perl perl-doc fcgiwrap
 
 # Change to server directory
 cd /mnt/server
